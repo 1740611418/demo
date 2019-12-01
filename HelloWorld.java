ed0a315f2116917bf4c885b36d1c272fbf940f8b
1740611418 <1063424699@qq.com>
2019-12-01 17:58:18 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public statuc void main(String args[]){
+		System.out.println("HelloWorld");
+		System.out.println("1");
+		System.out.println("HelloWorld");
+		System.out.println("2");
+	}
+}
